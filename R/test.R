d815e3a5e01be526b65096e42ff9c480a6d9f6c7
Zhichao Xu <xu@Xus-MacBook-Pro.attlocal.net>
2023-12-05 17:20:38 -0600

    Initial package commit

---
+++ R/test.R
@@ -0,0 +1,18 @@
+#' test.R
+#'
+#' Calculate the test statistic and p-value for interval-censored competing risks SKAT.
+#'
+#' @param leftDmat n*(p+nknots+2) design matrix for left end of
+#'
+#' @return A list with the elements:
+#'
+#' @importFrom stats rnorm
+#'
+#' @export
+#' @examples
+#' p0 <- 20
+test <- function(seed=1) {
+  x <- stats::rnorm(100,0,1)
+
+
+}
