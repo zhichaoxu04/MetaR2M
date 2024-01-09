72f40adad78e5f988aa28342c47eeb305ffdceb5
Zhichao Xu <xu@Xus-MacBook-Pro.attlocal.net>
2024-01-09 00:31:38 -0600

    update r files

--- R/R2_Meta.R
+++ R/R2_Meta.R
@@ -28,9 +28,9 @@
 #' Study <- c(1,2,3,4,5)
 #' Effects <- c(0.1,0.2,0.3,0.4,1.5)
 #' SE <- c(0.01, 0.02, 0.01, 0.08, 0.1)
-#' Method <- "fixed"
+#' Method <- "Fixed"
 #' MetaR2M::R2_Meta(Effects=Effects, Study=Study, SE=SE, Method=Method)
-#' Method <- "REML"
+#' Method <- "DL"
 #' MetaR2M::R2_Meta(Effects=Effects, Study=Study, SE=SE, Method=Method)
 R2_Meta <- function(Effects, Study=NULL, SE, Method=c("Fixed", "REML", "PM", "DL", "ML")){
   if(is.null(Study)){
@@ -41,7 +41,15 @@ R2_Meta <- function(Effects, Study=NULL, SE, Method=c("Fixed", "REML", "PM", "DL
 
   # Construct the data frame for the meta-analysis
   resultDF_S <- data.frame(Study = study_names, EstR2_S_tem = Effects, EstSD_S_tem = SE)
-  maFixed <- meta::metagen(TE=resultDF_S$EstR2_S_tem, seTE=resultDF_S$EstSD_S_tem, studlab=resultDF_S$Study, method.tau = Method)
+
+
+  if(Method == "Fixed"){
+    Method_tem <- "DL"
+  }else{
+    Method_tem <- Method
+  }
+
+  maFixed <- meta::metagen(TE=resultDF_S$EstR2_S_tem, seTE=resultDF_S$EstSD_S_tem, studlab=resultDF_S$Study, method.tau = Method_tem)
   # maFixed <- mvmeta::mvmeta(resultDF_S$EstR2_S_tem ~ 1, S=resultDF_S$EstSD_S_tem^2, method=Method)
 
   if(Method == "Fixed"){
@@ -50,17 +58,25 @@ R2_Meta <- function(Effects, Study=NULL, SE, Method=c("Fixed", "REML", "PM", "DL
     meta_CI_upper <- as.numeric(maFixed$upper.common)
     Q <- as.numeric(maFixed$Q)
     Q_pval <- as.numeric(maFixed$pval.Q)
+    if(Q_pval <= 0.05){
+      message("p value of Q is <= 0.05, try random-effects model")
+    }
 
 
   }else{
     meta_effect <- as.numeric(maFixed$TE.random)
     meta_CI_lower <- as.numeric(maFixed$lower.random)
     meta_CI_upper <- as.numeric(maFixed$upper.random)
+    Q <- as.numeric(maFixed$Q)
+    Q_pval <- as.numeric(maFixed$pval.Q)
+    if(Q_pval > 0.05){
+      message("p value of Q is <> 0.05, try fixed-effects model")
+    }
   }
 
 
 
-  return(c(meta_effect=meta_effect, meta_CI_lower=meta_CI_lower, meta_CI_upper=meta_CI_upper, Method=Method))
+  return(c(meta_effect=meta_effect, meta_CI_lower=meta_CI_lower, meta_CI_upper=meta_CI_upper, Q=Q, Q_pval=Q_pval, Method=Method))
 }
 
 
