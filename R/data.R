6e2c733f603cfad671a04b5e27aff7d5567bdaab
Zhichao Xu <xu@Xus-MacBook-Pro.attlocal.net>
2024-01-09 22:23:43 -0600

    create the example

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' @title Simulated data using Gompertz distribution
+#'
+#' @description simulated time-to-event outcome and event type
+#'
+#' @format A list of 2 with 1000 observations
+#' \describe{
+#'   \item{tempTime}{The time-to-event observed time.}
+#'   \item{tempType}{The type of causes to the outcome: primary outcome (1) or competing event (2).}
+#' }
+#' @references <https://onlinelibrary.wiley.com/doi/10.1111/biom.12109>
+"exampleData"
+#
+# N <- 2000
+# Outcome <- rnorm(N, 100, sd = 15)
+# Exposure <- rnorm(N, 60, sd = 10)
+# M <- matrix(0, nrow = N, ncol = 50)
+# for (i in 1:ncol(M)){M[, i] <- rnorm(N, 0, 1) * Exposure + rnorm(N, 0, 1)}
+# colnames(M) <- paste0("M_", 1:50)
+# exampleData <- list(study1 = cbind(data.frame(Y = Outcome[1: 300], X = Exposure[1: 300]), M[1:300, ]),
+#                     study2 = cbind(data.frame(Y = Outcome[301: 800], X = Exposure[301: 800]), M[301: 800, ]),
+#                     study3 = cbind(data.frame(Y = Outcome[801: 1200], X = Exposure[801: 1200]), M[801: 1200, ]),
+#                     study4 = cbind(data.frame(Y = Outcome[1201: 2000], X = Exposure[1201: 2000]), M[1201: 2000, ]))
+# usethis::use_data(exampleData, overwrite = TRUE)
